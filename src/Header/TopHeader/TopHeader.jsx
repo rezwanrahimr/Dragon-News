9a66e6b75d94b104637e42689823c273ce12ecac
rezwanrahimr <rezwanrahim69@gmail.com>
2023-04-27 10:42:02 +0600

    top header done

--- src/Header/TopHeader/TopHeader.jsx
+++ src/Header/TopHeader/TopHeader.jsx
@@ -1,7 +1,24 @@
+import { Button } from "react-bootstrap";
+import logo from "../../assets/logo.png";
+import "./topHeader.css";
+
 const TopHeader = () => {
   return (
-    <div>
-      <h1>Top HEader</h1>
+    <div className="mx-auto text-center top-header-container">
+      <div className="top-logo">
+        <img src={logo} alt="News Logo" /> <br />
+        <small>Journalism Without Fear or Favour</small>
+        <p>Sunday, November 27, 2022</p>
+      </div>
+      <div className="latest-container d-flex align-items-center ">
+        <Button variant="danger" className="mx-3">
+          Latest
+        </Button>{" "}
+        <p>
+          Match Highlights: Germany vs Spain — as it happened ! Match
+          Highlights: Germany vs Spain as...
+        </p>
+      </div>
     </div>
   );
 };
